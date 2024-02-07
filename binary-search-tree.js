a67b18b90abeba9a835793a305756d4bfbf2a548
royce <royce20002006@yahoo.com>
2024-02-07 00:50:57 -0600

    finished recursive transversals

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -70,17 +70,37 @@ class BinarySearchTree {
 
 
   preOrderTraversal(currentNode = this.root) {
-    // Your code here
+    if (currentNode) {
+      console.log(currentNode.val) 
+      this.preOrderTraversal(currentNode.left);
+      this.preOrderTraversal(currentNode.right);
+    }
+    
   }
 
 
   inOrderTraversal(currentNode = this.root) {
-    // Your code here
+    if (currentNode) {
+      this.inOrderTraversal(currentNode.left) 
+      console.log(currentNode.val)
+      this.inOrderTraversal(currentNode.right);
+
+    }
+    
   }
 
 
   postOrderTraversal(currentNode = this.root) {
     // Your code here
+    if (currentNode) {
+      this.postOrderTraversal(currentNode.left);
+      this.postOrderTraversal(currentNode.right);
+      console.log(currentNode.val)
+    }
+          
+      
+          
+      
   }
 
   // Breadth First Traversal - Iterative
